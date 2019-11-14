7ae1f246e0784eef42c7824eb761ba793b65caa8
Achim Gsell <achim.gsell@psi.ch>
2019-11-14 17:06:04 +0100

    fixes in config files

--- config-gcc-openmpi.sh
+++ config-gcc-openmpi.sh
@@ -1,6 +1,6 @@
 export TOOLSET=gcc
 export MPI_IMPLEMENTATION=openmpi
-export PREFIX="${HOME}/OPAL-gcc-openmpi"
+export PREFIX="${PREFIX:=${HOME}/OPAL-gcc-openmpi}"
 
 test -r "$(dirname '${BASH_SOURCE}')/setup.sh" && source "$_"
 
--- config-gcc920-openmpi.sh
+++ config-gcc920-openmpi.sh
@@ -1,7 +1,6 @@
 export TOOLSET=gcc
 export MPI_IMPLEMENTATION=openmpi
-export PREFIX="${HOME}/OPAL-gcc920-openmpi"
-
+export PREFIX="${PREFIX:=${HOME}/OPAL-gcc920-openmpi}"
 export GCC_VERSION=9.2.0
 
 test -r "$(dirname '${BASH_SOURCE}')/setup.sh" && source "$_"
