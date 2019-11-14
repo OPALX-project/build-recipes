e49603eec2d6534f681b77f50d398215a4e04306
Achim Gsell <achim.gsell@psi.ch>
2019-11-14 17:06:49 +0100

    more fixes in config files

--- config-clang-openmpi.sh
+++ config-clang-openmpi.sh
@@ -1,6 +1,6 @@
 export TOOLSET=clang
 export MPI_IMPLEMENTATION=openmpi
 
-export PREFIX="${HOME}/OPAL-clang-openmpi"
+export PREFIX="${PREFIX:-${HOME}/OPAL-clang-openmpi}"
 
 test -r "$(dirname '${BASH_SOURCE}')/setup.sh" && source "$_"
